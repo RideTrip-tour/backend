5892f2d112b0d0c256628deb1fa198ceae2d2e6a
Badmajor <v.balonkin@gmail.com>
2024-08-17 11:55:03 +0300

    add start container

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ from sqlalchemy import URL
 # DATABASE
 DATABASE_DIALECT = os.getenv("DATABASE_DIALECT", "postgresql")
 DATABASE_DRIVER = os.getenv("DATABASE_DRIVER", "asyncpg")
-DATABASE_NAME = os.getenv("POSTGRES_DB", "rtt_dev")
+DATABASE_NAME = os.getenv("POSTGRES_DB", "rtt_test")
 DATABASE_USER = os.getenv("POSTGRES_USER", "admin_test")
 DATABASE_PASS = os.getenv("POSTGRES_PASSWORD", "postgre_admin")
 DATABASE_HOST = os.getenv("POSTGRES_HOST", "localhost")
